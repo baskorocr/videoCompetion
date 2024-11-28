a6133283f217d0327be086f20aa93a85196379a4
Mahesa <baskorocr@gmail.com>
2024-11-28 13:34:43 +0700

    add description

--- resources/views/users/karya.blade.php
+++ resources/views/users/karya.blade.php
@@ -22,7 +22,7 @@
                             class="mt-6 text-gray-600 text-lg grid grid-cols-1 md:grid-cols-[150px,1fr] gap-4 text-justify">
                             <p class="font-semibold">Deskripsi:</p>
                             <p class="break-words">
-                                dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+                                {{ $karya->description }}
                             </p>
                         </div>
 
